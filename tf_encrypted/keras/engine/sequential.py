7aedcd77b118379401c112f222911045bf7d48c8
jvmancuso <jason@dropoutlabs.com>
2019-05-24 12:22:14 -0400

    stash

--- tf_encrypted/keras/engine/sequential.py
+++ tf_encrypted/keras/engine/sequential.py
@@ -38,25 +38,25 @@ class Sequential(Layer):
       if isinstance(layer, InputLayer):
         raise ValueError("Do not manually define an InputLayer in your "
                          "tfe.keras.Sequential model.")
-      else:
-        batch_shape = layer._batch_input_shape  # pylint: disable=protected-access
 
-        # Instantiate an input layer.
-        x = Input(
-            batch_shape=batch_shape,
-            name=layer.name + '_input')
-        # This will build the current layer
-        # and create the node connecting the current layer
-        # to the input layer we just created.
-        y = [layer(x)]
+      batch_shape = layer._batch_input_shape  # pylint: disable=protected-access
 
-        # If an input layer (placeholder) is available.
-        if isinstance(y, (tuple, list)):
-          raise ValueError('All layers in a Sequential model '
-                           'should have a single output tensor. '
-                           'For multi-output layers, '
-                           'use the functional API.')
-        self.outputs = [y]
+      # Instantiate an input layer.
+      x = Input(
+          batch_shape=batch_shape,
+          name=layer.name + '_input')
+      # This will build the current layer
+      # and create the node connecting the current layer
+      # to the input layer we just created.
+      y = [layer(x)]
+
+      # If an input layer (placeholder) is available.
+      if isinstance(y, (tuple, list)):
+        raise ValueError('All layers in a Sequential model '
+                         'should have a single output tensor. '
+                         'For multi-output layers, '
+                         'use the functional API.')
+      self.outputs = [y]
 
     elif self.outputs:
       # If the model is being built continuously on top of an input layer:
@@ -73,7 +73,23 @@ class Sequential(Layer):
     else:
       self._layers.append(layer)
 
-  def pop(self): """Return last layer in a model."""
+  def call(self, inputs, training=None, mask=None):  # pylint: disable=arguments-differ
+    if training is not None:
+      raise NotImplementedError()
+    if mask is not None:
+      raise NotImplementedError()
+    outputs = inputs  # handle the corner case where self.layers is empty
+    for layer in self.layers:
+      # During each iteration, `inputs` are the inputs to `layer`, and `outputs`
+      # are the outputs of `layer` applied to `inputs`. At the end of each
+      # iteration `inputs` is set to `outputs` to prepare for the next layer.
+      outputs = layer(inputs, **kwargs)
+
+      # `outputs` will be the inputs to the next layer.
+      inputs = outputs
+
+    return outputs
+
 
   @property
   def layers(self):
